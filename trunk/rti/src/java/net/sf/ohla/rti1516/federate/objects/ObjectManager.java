1d2759d2e2b4e1db89bbd2de04132eedfb268e44
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2007-02-15 21:33:17 +0000

    - updated copyright - removed unused variable

--- trunk/rti/src/java/net/sf/ohla/rti1516/federate/objects/ObjectManager.java
+++ trunk/rti/src/java/net/sf/ohla/rti1516/federate/objects/ObjectManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2006-2007, Michael Newcomb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.sf.ohla.rti1516.federate.objects;
 
 import java.util.ArrayList;
@@ -109,9 +125,6 @@ public class ObjectManager
     new HashMap<ObjectClassHandle, Set<ObjectInstanceHandle>>();
   protected Set<ObjectInstanceHandle> removedObjects =
     new HashSet<ObjectInstanceHandle>();
-
-  protected ConcurrentMap<ObjectInstanceHandle, ObjectClassHandle> reflectedObjectClassHandles =
-    new ConcurrentHashMap<ObjectInstanceHandle, ObjectClassHandle>();
 
   public ObjectManager(Federate federate)
   {
