86757f25e18a8bf44c43ea6827a618fb10a1567e
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2007-02-20 21:03:22 +0000

    - removed duplicate SubscriptionManager left over from a refactor

--- rti/src/java/net/sf/ohla/rti1516/federate/objects/ObjectManager.java
+++ rti/src/java/net/sf/ohla/rti1516/federate/objects/ObjectManager.java
@@ -29,10 +29,10 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import net.sf.ohla.rti1516.OHLAAttributeHandleSet;
+import net.sf.ohla.rti1516.SubscriptionManager;
 import net.sf.ohla.rti1516.fdd.InteractionClass;
 import net.sf.ohla.rti1516.fdd.ObjectClass;
 import net.sf.ohla.rti1516.federate.Federate;
-import net.sf.ohla.rti1516.federate.SubscriptionManager;
 import net.sf.ohla.rti1516.messages.DeleteObjectInstance;
 import net.sf.ohla.rti1516.messages.RegisterObjectInstance;
 import net.sf.ohla.rti1516.messages.ReserveObjectInstanceName;
