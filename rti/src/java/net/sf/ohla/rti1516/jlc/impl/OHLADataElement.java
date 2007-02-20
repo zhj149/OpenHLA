534ee90845ada8231d6bacb474b34b1be086eac5
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2007-02-20 21:41:49 +0000

    - refactoring

---
+++ rti/src/java/net/sf/ohla/rti1516/jlc/impl/OHLADataElement.java
@@ -0,0 +1,15 @@
+package net.sf.ohla.rti1516.jlc.impl;
+
+import hla.rti1516.jlc.ByteWrapper;
+import hla.rti1516.jlc.DataElement;
+
+public abstract class OHLADataElement
+  implements DataElement
+{
+  public byte[] toByteArray()
+  {
+    ByteWrapper byteWrapper = new ByteWrapper(getEncodedLength());
+    encode(byteWrapper);
+    return byteWrapper.array();
+  }
+}
