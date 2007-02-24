d073100e0431a6b68c6e6efe6eaf471d06dc9880
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2007-02-24 03:19:49 +0000

    - updated copyright

--- rti/src/java/net/sf/ohla/rti/federate/Federate.java
+++ rti/src/java/net/sf/ohla/rti/federate/Federate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, Michael Newcomb
+ * Copyright (c) 2006-2007, Michael Newcomb
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
