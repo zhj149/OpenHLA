72a2db112ba0e5e9baea9fd077e341c549558240
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2011-06-20 02:52:58 +0000

    - reworking Time Management tests

---
+++ trunk/testsuite/src/java/net/sf/ohla/rti/testsuite/hla/rti/time/TimeAdvanceRequestTestNG.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2005-2011, Michael Newcomb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.ohla.rti.testsuite.hla.rti.time;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import hla.rti.SuppliedParameters;
+
+@Test
+public class TimeAdvanceRequestTestNG
+  extends BaseTimeManagementTestNG
+{
+  private static final String FEDERATION_NAME = "OHLA IEEE 1516e Time Advance Request Test Federation";
+
+  private SuppliedParameters testSuppliedParameters;
+
+  public TimeAdvanceRequestTestNG()
+    throws Exception
+  {
+    super(2, FEDERATION_NAME);
+  }
+
+  @BeforeClass
+  public void setup()
+    throws Exception
+  {
+    rtiAmbassadors.get(0).enableTimeRegulation(initial, lookahead1);
+    rtiAmbassadors.get(1).enableTimeRegulation(initial, lookahead1);
+
+    federateAmbassadors.get(0).checkTimeRegulationEnabled(initial);
+    federateAmbassadors.get(1).checkTimeRegulationEnabled(initial);
+
+    rtiAmbassadors.get(0).enableTimeConstrained();
+    rtiAmbassadors.get(1).enableTimeConstrained();
+
+    federateAmbassadors.get(0).checkTimeConstrainedEnabled(initial);
+    federateAmbassadors.get(1).checkTimeConstrainedEnabled(initial);
+
+    int testInteractionClassHandle = rtiAmbassadors.get(0).getInteractionClassHandle(TEST_INTERACTION);
+
+    int parameterHandle1 = rtiAmbassadors.get(0).getParameterHandle(PARAMETER1, testInteractionClassHandle);
+    int parameterHandle2 = rtiAmbassadors.get(0).getParameterHandle(PARAMETER2, testInteractionClassHandle);
+    int parameterHandle3 = rtiAmbassadors.get(0).getParameterHandle(PARAMETER3, testInteractionClassHandle);
+
+    testSuppliedParameters = rtiFactory.createSuppliedParameters();
+    testSuppliedParameters.add(parameterHandle1, PARAMETER1_VALUE.getBytes());
+    testSuppliedParameters.add(parameterHandle2, PARAMETER2_VALUE.getBytes());
+    testSuppliedParameters.add(parameterHandle3, PARAMETER3_VALUE.getBytes());
+
+    rtiAmbassadors.get(0).publishInteractionClass(testInteractionClassHandle);
+
+    rtiAmbassadors.get(1).subscribeInteractionClass(testInteractionClassHandle);
+
+    setupComplete(federateAmbassadors);
+
+    rtiAmbassadors.get(0).sendInteraction(testInteractionClassHandle, testSuppliedParameters, TAG, five);
+    rtiAmbassadors.get(0).sendInteraction(testInteractionClassHandle, testSuppliedParameters, TAG, ten);
+  }
+
+  @Test
+  public void testTimeAdvanceRequest()
+    throws Exception
+  {
+    // advance federate 0 to 10, this will make both messages available to federate 1
+    //
+    rtiAmbassadors.get(0).timeAdvanceRequest(ten);
+
+    // advance federate 1 to 4
+    //
+    rtiAmbassadors.get(1).timeAdvanceRequest(four);
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(four);
+
+    // nothing should be received yet
+    //
+    federateAmbassadors.get(1).checkParameterValuesNotReceived();
+
+    // advance federate 1 to 5
+    //
+    rtiAmbassadors.get(1).timeAdvanceRequest(five);
+
+    // the first message should be received
+    //
+    federateAmbassadors.get(1).checkReceivedInteraction(testSuppliedParameters, five);
+
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(five);
+
+    // advance federate 1 to 4
+    //
+    rtiAmbassadors.get(1).timeAdvanceRequest(nine);
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(nine);
+
+    // nothing should be received yet
+    //
+    federateAmbassadors.get(1).checkParameterValuesNotReceived();
+
+    // advance federate 1 to 10
+    //
+    rtiAmbassadors.get(1).timeAdvanceRequest(ten);
+
+    // the second message should be received
+    //
+    federateAmbassadors.get(1).checkReceivedInteraction(testSuppliedParameters, ten);
+
+    federateAmbassadors.get(0).checkTimeAdvanceGrant(ten);
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(ten);
+  }
+}
---
+++ trunk/testsuite/src/java/net/sf/ohla/rti/testsuite/hla/rti1516e/time/TimeAdvanceRequestAvailableTestNG.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2005-2011, Michael Newcomb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.ohla.rti.testsuite.hla.rti1516e.time;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import hla.rti1516e.InteractionClassHandle;
+import hla.rti1516e.ParameterHandle;
+import hla.rti1516e.ParameterHandleValueMap;
+
+@Test
+public class TimeAdvanceRequestAvailableTestNG
+  extends BaseTimeManagementTestNG
+{
+  private static final String FEDERATION_NAME = "OHLA IEEE 1516e Time Advance Request Available Test Federation";
+
+  private InteractionClassHandle testInteractionClassHandle;
+
+  private ParameterHandleValueMap testParameterValues;
+
+  public TimeAdvanceRequestAvailableTestNG()
+    throws Exception
+  {
+    super(2, FEDERATION_NAME);
+  }
+
+  @BeforeClass
+  public void setup()
+    throws Exception
+  {
+    rtiAmbassadors.get(0).enableTimeRegulation(lookahead1);
+    rtiAmbassadors.get(1).enableTimeRegulation(lookahead1);
+
+    federateAmbassadors.get(0).checkTimeRegulationEnabled(initial);
+    federateAmbassadors.get(1).checkTimeRegulationEnabled(initial);
+
+    rtiAmbassadors.get(0).enableTimeConstrained();
+    rtiAmbassadors.get(1).enableTimeConstrained();
+
+    federateAmbassadors.get(0).checkTimeConstrainedEnabled(initial);
+    federateAmbassadors.get(1).checkTimeConstrainedEnabled(initial);
+
+    testInteractionClassHandle = rtiAmbassadors.get(0).getInteractionClassHandle(TEST_INTERACTION);
+
+    ParameterHandle parameterHandle1 = rtiAmbassadors.get(0).getParameterHandle(testInteractionClassHandle, PARAMETER1);
+    ParameterHandle parameterHandle2 = rtiAmbassadors.get(0).getParameterHandle(testInteractionClassHandle, PARAMETER2);
+    ParameterHandle parameterHandle3 = rtiAmbassadors.get(0).getParameterHandle(testInteractionClassHandle, PARAMETER3);
+
+    testParameterValues = rtiAmbassadors.get(0).getParameterHandleValueMapFactory().create(3);
+    testParameterValues.put(parameterHandle1, PARAMETER1_VALUE.getBytes());
+    testParameterValues.put(parameterHandle2, PARAMETER2_VALUE.getBytes());
+    testParameterValues.put(parameterHandle3, PARAMETER3_VALUE.getBytes());
+
+    rtiAmbassadors.get(0).publishInteractionClass(testInteractionClassHandle);
+
+    rtiAmbassadors.get(1).subscribeInteractionClass(testInteractionClassHandle);
+
+    setupComplete(federateAmbassadors);
+
+    rtiAmbassadors.get(0).sendInteraction(testInteractionClassHandle, testParameterValues, TAG, five);
+  }
+
+  @Test
+  public void testNextMessageRequestAvailable()
+    throws Exception
+  {
+    // advance federate 0 to 4, his LOTS will be 5 which will release the first message
+    //
+    rtiAmbassadors.get(0).timeAdvanceRequest(four);
+
+    // advance federate 1 to 5
+    //
+    rtiAmbassadors.get(1).timeAdvanceRequestAvailable(five);
+
+    // the first message should be received
+    //
+    federateAmbassadors.get(1).checkParameterValues(testParameterValues, five);
+
+    federateAmbassadors.get(0).checkTimeAdvanceGrant(four);
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(five);
+
+    // send another message at 5
+    //
+    rtiAmbassadors.get(0).sendInteraction(testInteractionClassHandle, testParameterValues, TAG, five);
+
+    rtiAmbassadors.get(0).timeAdvanceRequest(ten);
+    rtiAmbassadors.get(1).timeAdvanceRequest(ten);
+
+    // the first message should be received
+    //
+    federateAmbassadors.get(1).checkParameterValues(testParameterValues, five);
+
+    federateAmbassadors.get(0).checkTimeAdvanceGrant(ten);
+    federateAmbassadors.get(1).checkTimeAdvanceGrant(ten);
+  }
+}
