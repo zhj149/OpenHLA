e7a4c4d9e543f53a679c5b78f260c98d9e5e8b09
mnewcomb <mnewcomb@c6f40f97-f50e-0410-af12-a330f67be530>
2011-06-21 01:13:27 +0000

    - tweaking Time Management test organization

--- trunk/testsuite/src/java/net/sf/ohla/rti/testsuite/hla/rti1516e/time/TimeAdvanceRequestAvailableTestNG.java
+++ trunk/testsuite/src/java/net/sf/ohla/rti/testsuite/hla/rti1516e/time/TimeAdvanceRequestAvailableTestNG.java
@@ -22,6 +22,7 @@ import org.testng.annotations.Test;
 import hla.rti1516e.InteractionClassHandle;
 import hla.rti1516e.ParameterHandle;
 import hla.rti1516e.ParameterHandleValueMap;
+import hla.rti1516e.exceptions.LogicalTimeAlreadyPassed;
 
 @Test
 public class TimeAdvanceRequestAvailableTestNG
@@ -107,5 +108,22 @@ public class TimeAdvanceRequestAvailableTestNG
 
     federateAmbassadors.get(0).checkTimeAdvanceGrant(ten);
     federateAmbassadors.get(1).checkTimeAdvanceGrant(ten);
+  }
+
+  @Test(dependsOnMethods = { "testTimeAdvanceRequestAvailable" }, expectedExceptions = { LogicalTimeAlreadyPassed.class })
+  public void testTimeAdvanceRequestAvailableToLogicalTimeAlreadyPassed()
+    throws Exception
+  {
+    rtiAmbassadors.get(0).timeAdvanceRequestAvailable(five);
+  }
+
+  @Test(dependsOnMethods = { "testTimeAdvanceRequestAvailable" })
+  public void testTimeAdvanceRequestAvailableToSameTime()
+    throws Exception
+  {
+    rtiAmbassadors.get(0).timeAdvanceRequestAvailable(ten);
+    federateAmbassadors.get(0).checkTimeAdvanceGrant(ten);
+
+    assert ten.equals(rtiAmbassadors.get(0).queryLogicalTime());
   }
 }
